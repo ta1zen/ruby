cafed40b322227d02d9b91114934cb3ad99500aa
Andrei Tsuhui <fashiron228@gmail.com>
2026-05-22 12:12:26 +0300

    fix rubocop

--- lab3/config/routes.rb
+++ lab3/config/routes.rb
@@ -10,5 +10,4 @@ Rails.application.routes.draw do
 
   get 'service-worker' => 'rails/pwa#service_worker', as: :pwa_service_worker
   get 'manifest' => 'rails/pwa#manifest', as: :pwa_manifest
-
 end
